afaf2cd6a12bcea52aab497a1727ee6623971f64
kristian1108 <krlgaylord@gmail.com>
2020-01-09 02:13:53 -0500

    requirements

--- sms_utils.py
+++ sms_utils.py
@@ -3,9 +3,11 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
 	return "<h1 style='color:blue'>Hello There!</h1>"
+
 
 @app.route('/sms', methods=['GET', 'POST'])
 def sms_reply():
@@ -13,6 +15,7 @@ def sms_reply():
 	resp.message("Hola")
 
 	return str(resp)
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
