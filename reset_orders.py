872c2481a05594f5a201625aa4abb425287d64fb
Kristian Gaylord <kristian@pilots-MacBook-Pro.local>
2021-01-16 21:24:09 -0500

    cleanup

--- reset_orders.py
+++
@@ -1,5 +0,0 @@
-import api_utils as api
-
-if __name__ == "__main__":
-    ts = api.TradingSession()
-    api.TradingSession.close_open_orders()
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from sms_utils import app
-
-if __name__ == "__main__":
-	app.run()
